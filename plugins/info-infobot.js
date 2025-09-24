449c46e581959b04b852d219d841284d940cc5aa
edwardshanti3-sketch <edwardshanti3@gmail.com>
2025-09-24 09:24:26 -0600

    Update info-infobot.js

--- plugins/info-infobot.js
+++ plugins/info-infobot.js
@@ -61,7 +61,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     }
     
     
-    let ownerInfo = '👑 xzzys26'
+    let ownerInfo = '👑 Isagi'
     try {
       if (global.owner && Array.isArray(global.owner) && global.owner.length > 0) {
         ownerInfo = `👑 @${global.owner[0].split('@')[0]}`
