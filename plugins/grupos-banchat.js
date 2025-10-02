003445a8966f590eca336d04336b15770560e991
GianPoolS <116328472+GianPoolS@users.noreply.github.com>
2025-10-02 01:57:32 -0500

    Actualizar grupos-banchat.js

--- plugins/grupos-banchat.js
+++ plugins/grupos-banchat.js
@@ -19,7 +19,7 @@ handler.tags = ['grupo'];
 handler.command = ['banchat'];
 
 handler.botuser = true;
-handler.admin = true;
+//handler.admin = true;
 //handler.group = true;
 
 export default handler;
